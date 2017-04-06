4068dfcb8279bb37a692f58bfc4b1875038eae39
ktung <ptung@protonmail.ch>
2017-04-06 23:01:15 +0200

    ex 32

--- src/main/java/jf/App.java
+++ src/main/java/jf/App.java
@@ -2,10 +2,19 @@ package jf;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Vector;
+
+import org.apache.commons.collections.bag.PredicatedBag;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.PredicateUtils;
 
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 public class App {
 	
@@ -23,15 +32,35 @@ public class App {
 			CSVReader reader = new CSVReader(new FileReader(filename));
 			List<String[]> myEntries = reader.readAll();
 			
+			Vector<String> out = new Vector<String>();
+			
 			for (String[] entry : myEntries) {
-				int nb = Integer.parseInt(entry[0]);
-				System.out.println(String.format("Nombre lu : %d", nb));
-				System.out.println(String.format("Nb max entre %d et %d est %d", myMax, nb, app.max(myMax, nb)));
+				List<String>  list = Arrays.asList(entry);
+				CollectionUtils.select(list, new MonPredicat(), out);
 			}
+			
+			System.out.println("OUT"+ out);
+			
+			String[] filteredData = out.toArray(new String[0]);
+			String filenameOut = "data-filtered.csv";
+			CSVWriter writer = new CSVWriter(new FileWriter(filenameOut), '\t');
+			writer.writeNext(filteredData);
+			writer.close();
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+    }
+    
+    private static class MonPredicat implements Predicate<String> {
+
+		@Override
+		public boolean evaluate(String str) {
+			int val = Integer.parseInt(str);
+			return val < 50;
+		}
+    	
     }
 }
