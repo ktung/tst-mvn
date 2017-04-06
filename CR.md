9428cd1cd4b89157627ec972c245f31718f5c5cd
ktung <ptung@protonmail.ch>
2017-04-06 22:41:29 +0200

    ex 25

--- CR.md
+++ CR.md
@@ -1,1 +1,4 @@
 https://github.com/ktung/tst-mvn.git
+
+16. Bah les tests passent pas noob
+24. La dépendance openCSV n'est pas incluse
--- src/main/java/jf/App.java
+++ src/main/java/jf/App.java
@@ -1,20 +1,37 @@
 package jf;
 
-/**
- * Hello world!
- *
- */
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+
 public class App {
 	
 	public int max(int a, int b) {
-//		return (a > b) ? a : b;
-		return a;
+		return (a > b) ? a : b;
+//		return a;
 	}
 	
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
     	App app = new App();
     	
-        System.out.println(app.max(4, 5));
+    	int myMax = 5;
+    	String filename = "data.csv";
+    	try {
+			CSVReader reader = new CSVReader(new FileReader(filename));
+			List<String[]> myEntries = reader.readAll();
+			
+			for (String[] entry : myEntries) {
+				int nb = Integer.parseInt(entry[0]);
+				System.out.println(String.format("Nombre lu : %d", nb));
+				System.out.println(String.format("Nb max entre %d et %d est %d", myMax, nb, app.max(myMax, nb)));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 }
